067e447c53caa5a3634fa76d8cbf1f8d5c8c3077
Duy Nguyen <vincentddn@gmail.com>
2012-07-17 09:05:28 -0700

    STS Template Project - MVC - Hello World

---
+++ CharityProj/src/main/java/org/buddy/charity/HomeController.java
@@ -0,0 +1,48 @@
+package org.buddy.charity;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+
+import org.cloudfoundry.runtime.env.CloudEnvironment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! the client locale is "+ locale.toString());
+		
+		CloudEnvironment env = new CloudEnvironment();
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		Map<String, Object> map = model.asMap();
+		if (env.getInstanceInfo()!=null) {
+			map.put("host", env.getInstanceInfo().getHost());
+			map.put("port", env.getInstanceInfo().getPort());
+		}
+		
+		return "home";
+	}
+	
+}
