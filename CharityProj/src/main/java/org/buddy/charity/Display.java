17512d66971dbce863409a9b31aa00183814bb40
Zhichao <lzc.cs.hust@gmail.com>
2012-07-28 20:33:35 -0400

    For Duy's benefit

--- CharityProj/src/main/java/org/buddy/charity/Display.java
+++ CharityProj/src/main/java/org/buddy/charity/Display.java
@@ -3,6 +3,54 @@ package org.buddy.charity;
 public class Display {
 	public String name; 
 	public String score; 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getScore() {
+		return score;
+	}
+
+	public void setScore(String score) {
+		this.score = score;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
 	public String location; 
 	public String description; 
 	public String category; 
